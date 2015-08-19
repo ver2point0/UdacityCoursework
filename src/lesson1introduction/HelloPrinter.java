6f3696082aa238816ec502f89b996e74037167ad
John McIntosh II <jmci10@hotmail.com>
2015-08-18 20:46:40 -0400

    made and ran basic program

---
+++ src/lesson1introduction/HelloPrinter.java
@@ -0,0 +1,9 @@
+package lesson1introduction;
+
+
+public class HelloPrinter {
+    public static void main(String[] args) {
+        // displays the String, "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
