b387d1d5cbbc31272b72500b84598cacb75a3a17
John McIntosh II <jmci10@hotmail.com>
2015-08-18 20:51:13 -0400

    made program to print "Hello" and "World" on separate lines

---
+++ src/lesson1introduction/HelloWorld.java
@@ -0,0 +1,11 @@
+package lesson1introduction;
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // displays the String, "Hello" and "World"
+        // on two separate lines to the console
+        System.out.println("Hello");
+        System.out.println("World");
+    }
+}
