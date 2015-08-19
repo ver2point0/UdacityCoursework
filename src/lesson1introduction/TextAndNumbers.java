32c29e567471536cc55a64cf997526df50dde211
John McIntosh II <jmci10@hotmail.com>
2015-08-18 20:55:04 -0400

    printing text and numbers

---
+++ src/lesson1introduction/TextAndNumbers.java
@@ -0,0 +1,10 @@
+package lesson1introduction;
+
+
+public class TextAndNumbers {
+    public static void main(String[] args) {
+        // displays 12 and 3 + 4 + 5 to the console
+        System.out.println(3 + 4 + 5);
+        System.out.println("3 + 4 + 5");
+    }
+}
