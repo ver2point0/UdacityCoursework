55ead0422d194a762e04f37f2b5e21d8828ce9bd
John McIntosh II <jmci10@hotmail.com>
2015-08-21 19:52:28 -0400

    algorithms

---
+++ src/lesson1introduction/Algorithms.java
@@ -0,0 +1,8 @@
+package lesson1introduction;
+
+public class Algorithms {
+    public static void main(String[] args) {
+        // program to compute which car is better to buy
+        // based on miles driven, gas mileage, and price per gallon
+    }
+}
