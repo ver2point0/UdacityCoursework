6b8412c0480a99fcf073944bb57120196d066584
John McIntosh II <jmci10@hotmail.com>
2015-08-28 14:02:41 -0400

    String's  replace() method demo

--- src/lesson2objects/ReplaceMethodDemo.java
+++ src/lesson2objects/ReplaceMethodDemo.java
@@ -3,11 +3,12 @@ package lesson2objects;
 // working with the replace method
 // replacing "i"s with "x"s
 public class ReplaceMethodDemo {
-    private static String river = "Mississippi";
-    private static String newRiver;
+
     public static void main(String[] args) {
+        String river = "Mississippi";
+        String newRiver;
         System.out.println("The current river is: " + river);
-        newRiver = river.replace('i', 'x');
+        newRiver = river.replace("i", "x");
         System.out.println("After replacing the 'i's with 'x's : " + newRiver);
     }
 }
