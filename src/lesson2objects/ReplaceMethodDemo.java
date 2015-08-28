e31e8ded971e43662c8681ec11e0656307c87d69
John McIntosh II <jmci10@hotmail.com>
2015-08-28 14:22:06 -0400

    replace() method is a mutator

--- src/lesson2objects/ReplaceMethodDemo.java
+++ src/lesson2objects/ReplaceMethodDemo.java
@@ -1,14 +1,21 @@
 package lesson2objects;
 
-// working with the replace method
-// replacing "i"s with "x"s
+
 public class ReplaceMethodDemo {
 
     public static void main(String[] args) {
+        // working with the replace method
+        // replacing "i"s with "x"s
         String river = "Mississippi";
         String newRiver;
         System.out.println("The current river is: " + river);
         newRiver = river.replace("i", "x");
         System.out.println("After replacing the 'i's with 'x's : " + newRiver);
+
+        // Is replace an accessor or mutator method?
+        String greeting = "Hello";
+        System.out.println(greeting.replace("H", "J"));
+        System.out.println(greeting);
+
     }
 }
