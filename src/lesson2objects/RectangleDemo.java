97b1a3e2d9c7a1858b98bc5f84a9c43feb5c8817
John McIntosh II <jmci10@hotmail.com>
2015-08-28 14:27:53 -0400

    translate() method is a mutator

--- src/lesson2objects/RectangleDemo.java
+++ src/lesson2objects/RectangleDemo.java
@@ -5,9 +5,19 @@ public class RectangleDemo
     public static void main(String[] args)
     {
         // making two boxes touch
-        Rectangle box = new Rectangle(60, 90, 20, 30);
-        Rectangle box2 = new Rectangle(80, 120, 20, 30);
-        box.draw();
-        box2.draw();
+//        Rectangle box = new Rectangle(60, 90, 20, 30);
+//        Rectangle box2 = new Rectangle(80, 120, 20, 30);
+//        box.draw();
+//        box2.draw();
+
+        // Is translate() an accessor or mutator method
+        // It's a mutator
+        Rectangle box = new Rectangle(5, 10, 60, 90);
+        System.out.println(box.getX());
+        System.out.println(box.getWidth());
+        box.translate(25, 40);
+        System.out.println(box.getX());
+        System.out.println(box.getWidth());
+
     }
 }
--- src/lesson2objects/ReplaceMethodDemo.java
+++ src/lesson2objects/ReplaceMethodDemo.java
@@ -6,16 +6,16 @@ public class ReplaceMethodDemo {
     public static void main(String[] args) {
         // working with the replace method
         // replacing "i"s with "x"s
-        String river = "Mississippi";
-        String newRiver;
-        System.out.println("The current river is: " + river);
-        newRiver = river.replace("i", "x");
-        System.out.println("After replacing the 'i's with 'x's : " + newRiver);
+//        String river = "Mississippi";
+//        String newRiver;
+//        System.out.println("The current river is: " + river);
+//        newRiver = river.replace("i", "x");
+//        System.out.println("After replacing the 'i's with 'x's : " + newRiver);
 
         // Is replace an accessor or mutator method?
+        // It's an accessor method
         String greeting = "Hello";
         System.out.println(greeting.replace("H", "J"));
         System.out.println(greeting);
-
     }
 }
