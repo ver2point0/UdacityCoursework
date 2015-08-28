31c60ef4dbc027ac69e2c5ef64b51ff98d0739f5
John McIntosh II <jmci10@hotmail.com>
2015-08-28 16:05:26 -0400

    drew a 3D box

---
+++ src/lesson2objects/DrawACrate.java
@@ -0,0 +1,20 @@
+package lesson2objects;
+
+public class DrawACrate {
+    public static void main(String[] args) {
+        // drawing a crate
+        Rectangle face = new Rectangle(20, 30, 100, 40);
+        face.draw();
+        Line line1 = new Line (20, 30, 50, 10);
+        line1.draw();
+        Line line2 = new Line(120, 70, 150, 50);
+        line2.draw();
+        Line line3 = new Line(120, 30, 150, 10);
+        line3.draw();
+        Line line4 = new Line(50, 10, 150, 10);
+        line4.draw();
+        Line line5 = new Line(150, 10, 150, 50);
+        line5.draw();
+    }
+
+}
