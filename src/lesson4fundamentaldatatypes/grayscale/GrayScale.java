1a8508ab425465323bf287a4c7c28610037b22f4
John McIntosh II <jmci10@hotmail.com>
2015-09-05 21:49:13 -0400

    use grayscale

---
+++ src/lesson4fundamentaldatatypes/grayscale/GrayScale.java
@@ -0,0 +1,18 @@
+package lesson4fundamentaldatatypes.grayscale;
+
+public class GrayScale
+{
+    public static void main(String[] args)
+    {
+        Picture pic = new Picture();
+        pic.load("eliza.jpg");
+        pic.draw();
+        for (int i = 0; i < pic.pixels(); i++)
+        {
+            Color c = pic.getColorAt(i);
+            c.turnGray();
+            pic.setColorAt(i, c);
+        }
+    }
+}
+
