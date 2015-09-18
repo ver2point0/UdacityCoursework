bf85bbd888cf58761bd91fcee9a2d0ba7fb058d7
John McIntosh II <jmci10@hotmail.com>
2015-09-17 20:04:18 -0400

    compare strings

--- src/lesson5dash2decisions/EqualNumbers.java
+++ src/lesson5dash2decisions/EqualNumbers.java
@@ -2,12 +2,15 @@ package lesson5dash2decisions;
 
 public class EqualNumbers {
     public static void main(String[] args) {
+        // When comparing two decimal numbers, compare whether they are close
+        // enough to each other, not if they are exactly the same
 
         double original = 2;
         double root = Math.sqrt(original);
         double rootSquared = root * root;
+        final double EPSILON = 1E-12;
 
-        if (rootSquared == original) {
+        if (Math.abs(rootSquared - original) < EPSILON) {
             System.out.println("They are the same");
         } else {
             System.out.println("rootSquared is " + rootSquared);
