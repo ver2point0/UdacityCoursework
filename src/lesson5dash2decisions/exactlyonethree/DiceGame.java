a72fa6d1a86984d591b5131e2db8bed757ad300f
John McIntosh II <jmci10@hotmail.com>
2015-09-19 03:22:43 -0400

    find one three

---
+++ src/lesson5dash2decisions/exactlyonethree/DiceGame.java
@@ -0,0 +1,23 @@
+package lesson5dash2decisions.exactlyonethree;
+
+public class DiceGame
+{
+    /**
+     * Calculates the score of a round of this game.
+     * x the first roll
+     * y the second roll
+     * @return a score of 1 if exactly one is 3, a score of 0 otherwise
+     */
+    public int exactlyOneThree(int x, int y)
+    {
+        if (x == 3 && y != 3 || x != 3 && y == 3) // if exactly one of x and y is 3
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+
+    }
+}
