28c89d6beff175eb03c72fdddbe21bfbba4bccf2
John McIntosh II <jmci10@hotmail.com>
2015-09-21 23:20:09 -0400

    change interest rate

--- src/lesson6dash1loops/billionaire2/InterestCalculator.java
+++ src/lesson6dash1loops/billionaire2/InterestCalculator.java
@@ -1,14 +1,14 @@
 package lesson6dash1loops.billionaire2;
 
 // Change the Program so that it will calculate the number of years
-// to reach 1 billion dollars when the interest rate is 10%
+// to reach 1 million dollars when the interest rate is 10%
 public class InterestCalculator
 {
     public static void main(String[] args)
     {
         double balance = 100;
         double rate = 0.10;
-        double target = 1000000000;
+        double target = 1000000;
         int year = 0;
 
         while (balance < target)
---
+++ src/lesson6dash1loops/creditcard/CreditCard.java
@@ -0,0 +1,55 @@
+package lesson6dash1loops.creditcard;
+
+// Implement the isValid method.
+public class CreditCard
+{
+    private long cardNumber;
+
+    public CreditCard(long cardNumber)
+    {
+        this.cardNumber = cardNumber;
+    }
+
+    /**
+     * Calculates whether this CreditCard has a valid number.
+     * @return true if the number is valid, false if it's not.
+     */
+    public boolean isValid()
+    {
+        /* Pseudocode for isValid:
+         * sum = 0
+         * count = 0
+         * for each digit starting from the right
+         *     count ++
+         *     if count is odd
+         *         sum = sum + digit
+         *     else if (digit < 5)
+         *         sum = sum + 2 * digit
+         *     else
+         *         sum = sum + 2 * digit - 9
+         * if the last digit of the sum is zero
+         *     The card number is valid
+         */
+        // this is the code from the last question. you can use it
+        // as a starting point, but you will need to change most of it.
+
+        long n = cardNumber;
+        int sum = 0;
+        int count = 0;
+        while (n > 0)
+        {
+            count++;
+            int digit = (int)(n % 10);
+
+            if (count % 2 == 1) {
+                sum = sum + digit;
+            } else if(digit < 5) {
+                sum = sum + 2 * digit;
+            } else {
+                sum = sum + 2 * digit - 9;
+            }
+            n = n / 10;
+        }
+        return sum % 10 == 0;
+    }
+}
