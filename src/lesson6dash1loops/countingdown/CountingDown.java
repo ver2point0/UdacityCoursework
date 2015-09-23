3947c5a101d2cb6feeda19fe94a6795c18bef8a6
John McIntosh II <jmci10@hotmail.com>
2015-09-23 02:40:48 -0400

    count down

---
+++ src/lesson6dash1loops/countingdown/CountingDown.java
@@ -0,0 +1,19 @@
+package lesson6dash1loops.countingdown;
+
+// Make the loop count down the days left until vacation.
+public class CountingDown
+{
+    public static void main(String[] args)
+    {
+        // Make this loop count down instead of up.
+        // It should print
+        //   20 days left
+        //   19 days left
+        //   18 days left
+        // and so on, down to 0 days left.
+        for (int i = 20; i > 0; i--)
+        {
+            System.out.println(i + " days left");
+        }
+    }
+}
