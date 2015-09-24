922550cda16f0eafe9e3093aff2a239c3a21dbfa
John McIntosh II <jmci10@hotmail.com>
2015-09-24 02:15:19 -0400

    decode a string message

---
+++ src/lesson6dash1loops/secretmessages/Decoder.java
@@ -0,0 +1,41 @@
+package lesson6dash1loops.secretmessages;
+
+/**
+ * To do: implement the decode method. 
+ */
+public class Decoder
+{
+    private String encodedMessage;
+
+    public Decoder(String encodedMessage)
+    {
+        this.encodedMessage = encodedMessage;
+    }
+
+    /**
+     * Reads every 10th letter of encodedMessage and returns a string 
+     * with the letters it reads. 
+     * @return 
+     */
+    public String decode()
+    {
+        String decodedMessage = "";
+        for (int i = 0; i < encodedMessage.length(); i = i + 10)
+        {
+            decodedMessage += encodedMessage.charAt(i);
+        }
+        return decodedMessage;
+    }
+    
+    /**
+     * Optional: 
+     * Decodes the same way as String decode() but takes an index to start at. 
+     * There may be more than one secret message in the 
+     */
+    public String decode(int startingIndex)
+    {
+        String decodedMessage = "";
+        // YOUR CODE HERE
+        return decodedMessage;
+    }
+}
