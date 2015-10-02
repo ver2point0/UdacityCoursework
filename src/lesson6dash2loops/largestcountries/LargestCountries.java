cda2a66d8dabd479153ae24464f00460d0d6ec2b
John McIntosh II <jmci10@hotmail.com>
2015-10-01 23:52:09 -0400

    read country names and populations from a file

---
+++ src/lesson6dash2loops/largestcountries/LargestCountries.java
@@ -0,0 +1,38 @@
+package lesson6dash2loops.largestcountries;// Bluej project: lesson6/largest_countries
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class LargestCountries
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        String filename = "C:\\Users\\PtJ\\UdacityCoursework\\src\\lesson6dash2loops\\largestcountries\\population.txt";
+        File inputFile = new File(filename);
+        Scanner in = new Scanner(inputFile);
+
+        String country = in.next();
+        String largestCountry = country;
+        double population = in.nextDouble();
+        double largest = population;
+
+        // Print the population and name of the country with largest population
+
+        // HINT: What else do you need to remember?
+        // What variable(s) should you add so you can
+        // print the population and the country name at the end?
+        while (in.hasNext())
+        {
+            country = in.next();
+            population = in.nextDouble();
+            if (population > largest)
+            {
+                largestCountry = country;
+                largest = population;
+            }
+        }
+        System.out.println("The country with the largest population is " + largestCountry);
+        System.out.println("The largest population is " + largest);
+    }
+}
