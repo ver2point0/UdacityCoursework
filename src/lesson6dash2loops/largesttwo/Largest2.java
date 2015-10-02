9002cce84b24d304deb2654e0156f77767869c97
John McIntosh II <jmci10@hotmail.com>
2015-10-01 23:34:41 -0400

    read the largest number from a file

---
+++ src/lesson6dash2loops/largesttwo/Largest2.java
@@ -0,0 +1,32 @@
+package lesson6dash2loops.largesttwo;
+
+// Read in the values from the file input.txt, stopping
+// at the end of the file (or if the file contains
+// an input that is not a number). Print out the largest
+// value that you read.
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Largest2
+{
+    public static void main(String[] args)
+    throws FileNotFoundException
+    {
+        String file = "C:\\Users\\PtJ\\UdacityCoursework\\src\\lesson6dash2loops\\largesttwo\\input.txt";
+        File inputFile = new File(file);
+        Scanner in = new Scanner(inputFile);
+
+        double largest = in.nextDouble();
+        while (in.hasNextDouble())
+        {
+            double input = in.nextDouble();
+            if (input > largest)
+            {
+                largest = input;
+            }
+        }
+        System.out.println("Largest value: " + largest);
+    }
+}
