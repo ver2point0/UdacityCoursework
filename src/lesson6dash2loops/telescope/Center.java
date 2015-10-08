1ab35eb7780f298749776882aabf00dd0c53e87a
John McIntosh II <jmci10@hotmail.com>
2015-10-07 23:59:47 -0400

    make telescop effect

---
+++ src/lesson6dash2loops/telescope/Center.java
@@ -0,0 +1,38 @@
+package lesson6dash2loops.telescope;// BlueJ project: lesson6/telescope
+
+// Your task is to color all pixels black that are outside the
+// telescope circle. The center and radius of the circle are
+// given below.
+
+// For each point, find out the distance to the center and
+// check if it is larger than the radius.
+
+// Hint: The distance between two points (x1, y1) and (x2, y2) is
+// Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
+
+public class Center
+{
+    public static void main(String[] args)
+    {
+        Picture pic = new Picture();
+        pic.load("C:\\Users\\PtJ\\UdacityCoursework\\src\\lesson6dash2loops\\telescope\\queen-mary.png");
+        pic.draw();
+
+        double centerX = pic.getWidth() / 2;
+        double centerY = pic.getHeight() / 2;
+        double radius = pic.getHeight() / 4;
+
+        for (int x = 0; x < pic.getWidth(); x++)
+        {
+            for (int y = 0; y < pic.getHeight(); y++)
+            {
+                double distance = Math.sqrt((centerX - x) * (centerX - x)
+                + (centerY - y) * (centerY - y));
+                if (distance > radius)
+                {
+                    pic.setColorAt(x, y, Color.BLACK);
+                }
+            }
+        }
+    }
+}
