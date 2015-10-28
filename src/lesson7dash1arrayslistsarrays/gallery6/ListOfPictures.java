4f8ffc194e55fff5a3b1768cb75e51611c0188db
John McIntosh II <jmci10@hotmail.com>
2015-10-28 11:11:45 -0400

    find the tallest picture in the gallery

---
+++ src/lesson7dash1arrayslistsarrays/gallery6/ListOfPictures.java
@@ -0,0 +1,29 @@
+package lesson7dash1arrayslistsarrays.gallery6;
+// BlueJ project: lesson7/gallery6
+
+import java.util.ArrayList;
+
+public class ListOfPictures
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Picture> gallery = new ArrayList<Picture>();
+        gallery.add(new Picture("C:\\Users\\PtJ\\UdacityCoursework\\src\\lesson7dash1arrayslistsarrays\\gallery6\\degas1.jpg"));
+        gallery.add(new Picture("C:\\Users\\PtJ\\UdacityCoursework\\src\\lesson7dash1arrayslistsarrays\\gallery6\\guigou1.jpg"));
+        gallery.add(new Picture("C:\\Users\\PtJ\\UdacityCoursework\\src\\lesson7dash1arrayslistsarrays\\gallery6\\gaugin1.jpg"));
+        gallery.add(new Picture("C:\\Users\\PtJ\\UdacityCoursework\\src\\lesson7dash1arrayslistsarrays\\gallery6\\monet1.jpg"));
+        gallery.add(new Picture("C:\\Users\\PtJ\\UdacityCoursework\\src\\lesson7dash1arrayslistsarrays\\gallery6\\seurat1.jpg"));
+
+        // Your code here
+        Picture tallest = gallery.get(0);
+        for (Picture pic : gallery) {
+            if (pic.getHeight() > tallest.getHeight()) {
+                tallest = pic;
+            }
+        }
+
+        System.out.println("Tallest height: "
+                           + tallest.getHeight());
+        tallest.draw();
+    }
+}
