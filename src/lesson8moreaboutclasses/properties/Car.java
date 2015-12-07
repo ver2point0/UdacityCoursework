c6849b5da620052908ff9018c15a8717a4b767ad
John McIntosh II <jmci10@hotmail.com>
2015-12-07 17:19:10 -0500

    implement getters and setters appropriately

---
+++ src/lesson8moreaboutclasses/properties/Car.java
@@ -0,0 +1,74 @@
+package lesson8moreaboutclasses.properties;
+
+// BlueJ project: lesson8/properties
+// Add the appropriate getter and setter methods for the instance variables.
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Car
+{
+    private int numberOfSeats; // Don't make this a settable property
+    private String driverName;
+    private ArrayList<String> passengers; // Not a property
+
+    /**
+        Constructs a car with a given number of seats
+        @param numberOfSeats the number of seats in this car.
+    */
+    public Car(int numberOfSeats)
+    {
+        this.numberOfSeats = numberOfSeats;
+        passengers = new ArrayList<String>();
+    }
+
+    /**
+       Adds a passenger to this car.
+       @param name the name of the passenger
+    */
+    public void addPassenger(String name)
+    {
+        passengers.add(name);
+    }
+
+    /*
+    * Gets the driver's name
+    * @return the name of the driver
+    * */
+    public String getDriverName() {
+        return driverName;
+    }
+
+    /*
+    * Sets the driver's name
+    * @param newName the new name of the driver
+    * */
+    public void setDriverName(String newName) {
+        if (newName.length() > 0) {
+            driverName = newName;
+        }
+    }
+
+    /**
+       Gets the number of occupants in this car.
+       @return the number of occupants
+    */
+    public int getOccupantCount()
+    {
+        int count = passengers.size();
+        if (driverName != null)
+        {
+            count++;
+        }
+        return count;
+    }
+
+    /**
+       Gets a list of all passengers in this car.
+       @return a list of the form [name1, name2, ...]
+    */
+    public String getPassengerList()
+    {
+        return passengers.toString();
+    }
+}
