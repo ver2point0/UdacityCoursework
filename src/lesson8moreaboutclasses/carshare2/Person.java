fc466ea2f9ac618bdcbb8bc7ad4d0644cda90dc6
John McIntosh II <jmci10@hotmail.com>
2015-12-09 22:43:45 -0500

    implement tryToAdd() and drive() methods

---
+++ src/lesson8moreaboutclasses/carshare2/Person.java
@@ -0,0 +1,28 @@
+package lesson8moreaboutclasses.carshare2;
+
+public class Person
+{
+    private String name;
+    private int destination;
+
+    public Person(String name, int destination)
+    {
+        this.name = name;
+        this.destination = destination;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getDestination()
+    {
+        return destination;
+    }
+
+    public String toString()
+    {
+        return name;
+    }
+}
