b21226b9d6ad69b4cdf5c5a7160f5249487ee871
John McIntosh II <jmci10@hotmail.com>
2016-01-18 20:03:02 -0500

    compute avearge of anything using a common interface

---
+++ src/lesson9interfacesinheritance/measure3/Measurable.java
@@ -0,0 +1,12 @@
+package lesson9interfacesinheritance.measure3;//HIDE
+/**
+   Describes any class whose objects can be measured.
+*/
+public interface Measurable
+{
+    /**
+       Computes the measure of the object.
+       @return the measure
+    */
+    double getMeasure();
+}
